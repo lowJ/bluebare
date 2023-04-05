c036d727fc5c0d2addca49691ac06b51c2a6b459
Jon <jwlow@uci.edu>
2023-04-04 19:11:21 -0700

    Uart tx in hal

--- Core/Inc/blue_hal.h
+++ Core/Inc/blue_hal.h
@@ -37,11 +37,11 @@ typedef enum {
 	LED_BLUE
 } bh_led_t;
 
-bool bh_init();
+bool bh_init(ADC_HandleTypeDef* adc1, TIM_HandleTypeDef* tim2, UART_HandleTypeDef* uart_handle);
 uint16_t bh_measure_dist(bh_dist_t dist);
 bool bh_set_motor_dir(bh_motor_t motor, bh_motor_dir_t dir);
 bool bh_set_motor_pwm(bh_motor_t motor, uint16_t dc);
-bool bh_uart_tx(uint8_t* buf, uint16_t num_bytes);
+bool bh_uart_tx_str(uint8_t* buf);
 bool bh_ble_tx(uint8_t *buf, uint16_t num_bytes);
 bool bh_set_led(bh_led_t led, bool state);
 bool bh_set_buzzer(uint16_t tone, bool state); /* TODO: Verify params */
